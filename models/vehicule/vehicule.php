411c9d021552854f35833a443ec37823360f6901
Jad-Bel <jadthegamer06@gmail.com>
2024-12-31 17:06:25 +0100

    add methods to retrieve, add, and update vehicule records in the database

--- models/vehicule/vehicule.php
+++ models/vehicule/vehicule.php
@@ -9,9 +9,63 @@ class vehicule {
         $this->conn = $db->getdatabase();
     }
 
-    public function ajouterVehicule() {
-        
+    public function affAllVehicule() {
+        $db = new Database();
+        $this->conn = $db->getdatabase();
+
+        $query = "SELECT * FROM vehicule";
+        $stmt = $this->conn->prepare($query);
+
+        $stmt->execute();
+
+        $vehicule = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $vehicule;
+    } 
+
+    public function ajouterVeh ($marque, $categorie, $disponibilite, $prix, $desc) {
+        $marque = htmlspecialchars($marque);
+        $categorie = htmlspecialchars($categorie);
+        $disponibilite = htmlspecialchars($disponibilite);
+        $prix = htmlspecialchars($prix);
+        $desc = htmlspecialchars($desc);
+
+        $query = "INSERT INTO vehicule (marque, disponibilite, prix, description) VALUES (:marque, :disp, :prix, :desc)";
+        $stmt = $this->conn->prepare($query);
+
+        $param = [":marque" => $marque,
+                    ":disp" => $disponibilite,
+                    ":prix" => $prix,
+                    ":desc" => $desc];
+
+        $stmt->execute($param);
     }
+
+    public function modifierVeh ($vehicule_id, $marque, $categorie, $disponibilite, $prix, $desc) {
+        $id = htmlspecialchars(intval($vehicule_id));
+        $marque = htmlspecialchars($marque);
+        $categorie = htmlspecialchars($categorie);
+        $disponibilite = htmlspecialchars($disponibilite);
+        $prix = htmlspecialchars($prix);
+        $desc = htmlspecialchars($desc);
+
+        $query = "UPDATE vehicule SET marque = :marque, disponibilite = :disp, prix = :prix, description = :desc WHERE vehicule_id = :id";
+        $stmt = $this->conn->prepare($query);
+
+        $param = [":id" => $vehicule_id,
+                ":marque" => $marque,
+                ":disp" => $disponibilite,
+                ":prix" => $prix,
+                ":desc" => $desc];
+
+        $result = $stmt->execute($param);
+        if (!$result) {
+            echo "Record updated successfully";
+        } else {
+            echo "Error updating record";
+        }
+    }
+
+    
 }
 
 ?>
