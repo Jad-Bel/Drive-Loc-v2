efbb98352a4826ae82589adc50637f2fb8d84c01
Jad-Bel <jadthegamer06@gmail.com>
2025-01-01 16:45:26 +0100

    FEAT(reservation): add method to fetch all reservations from the database

--- models/reservation/reservation.php
+++ models/reservation/reservation.php
@@ -8,4 +8,14 @@ class reservation {
         $db = new Database();
         $this->conn = $db->getdatabase();
     }
+
+    public function affAllReservation () {
+        $query = "SELECT * FROM reservation";
+        $stmt = $this->conn->prepare($query);
+
+        $stmt->execute();
+
+        $reservation = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $reservation;
+    }
 }
