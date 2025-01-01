9669fea720fa29dae042b1202494f4c7dbc585ea
Jad-Bel <jadthegamer06@gmail.com>
2025-01-01 16:22:24 +0100

    FEAT(categorie): add method to fetch all categories from the database

--- models/categorie/categorie.php
+++ models/categorie/categorie.php
@@ -7,4 +7,14 @@ class categorie {
         $db = new Database();
         $this->conn = $db->getdatabase();
     }
+
+    public function affAllCategorie() {
+        $query = "SELECT * FROM categorie";
+        $stmt = $this->conn->prepare($query);
+
+        $stmt->execute();
+
+        $categorie = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $categorie;
+    }
 }
