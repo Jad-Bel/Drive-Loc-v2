dad951aa353a844a47d267ee01d5b3d875ce8f61
Jad-Bel <jadthegamer06@gmail.com>
2025-01-01 16:11:24 +0100

    add error handling for adding and updating avis in the avis model

--- models/avis/avis.php
+++ models/avis/avis.php
@@ -23,15 +23,39 @@ class avis {
     }
 
     public function ajouterAvis ($user_id, $vehicule_id, $avis) {
-        $query = "INSERT INTO avis (user_id, vehicule_id, avis) VALUE (:user_id, :vehicule_id, :avis)";
-        $stmt = $this->conn->prepare($query);
+        try {
+            $avis = htmlspecialchars($avis);
+            $query = "INSERT INTO avis (user_id, vehicule_id, avis) VALUE (:user_id, :vehicule_id, :avis)";
+            $stmt = $this->conn->prepare($query);
+    
+            $param = [
+                        ":user_id" => $user_id,
+                        ":vehicule_id" => $vehicule_id,
+                        ":avis" => $avis];
+    
+            $stmt->execute($param);
+        } catch (Exception $e) {
+            throw new Error("cannot add avis:" . $e->getMessage());
+        }
+    }
 
-        $param = [
-                    ":user_id" => $user_id,
-                    ":vehicule_id" => $vehicule_id,
-                    ":avis" => $avis];
+    public function modifierAvis ($avis_id, $user_id, $vehicule_id, $avis) {
+        try {
+            $id = htmlspecialchars(intval($avis_id));
+            $query = "UPDATE avis SET user_id = :user_id, vehicule_id = :vehicule_id, avis = :avis WHERE avis_id = :id";
+            $stmt = $this->conn->prepare($query);
 
-        $stmt->execute($param);
+            $param = [
+                        ":user_id" => $user_id,
+                        ":vehicule_id" => $vehicule_id,
+                        ":avis" => $avis,
+                        ":id" => $id];
+
+            $stmt->execute($param);
+        } catch (Exception $e) {
+            throw new Error("cannot update avis:" . $e->getMessage());
+        }
+
     }
 
     public function suppAvis ($avis_id) {
