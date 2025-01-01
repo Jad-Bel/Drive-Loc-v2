f293a145170ca22a37f4e70a226fe61300167765
Jad-Bel <jadthegamer06@gmail.com>
2025-01-01 16:24:50 +0100

    FEAT(categorie): add method to insert a new category into the database

--- models/categorie/categorie.php
+++ models/categorie/categorie.php
@@ -21,4 +21,18 @@ class categorie {
             throw new Error("cannot get categorie:" . $e->getMessage());
         }
     }
+
+    public function ajouterCat ($ctg_name) {
+        try {
+            $ctg_name = htmlspecialchars($ctg_name);
+            $query = "INSERT INTO categorie (ctg_name) VALUES (:ctg)";
+            $stmt = $this->conn->prepare($query);
+
+            $param = [":ctg" => $ctg_name];
+
+            $stmt->execute($param);
+        } catch (Exception $e) {
+            throw new Error("cannot add categorie:" . $e->getMessage());
+        }
+    }
 }
