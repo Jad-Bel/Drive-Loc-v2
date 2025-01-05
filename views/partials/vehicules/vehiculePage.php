afbd575c0db4a32b09c06e948a6e1291b5f3c6a8
Jad-Bel <jadthegamer06@gmail.com>
2025-01-06 00:22:36 +0100

    FEAT: update contact information and social media links in vehicle page

--- views/partials/vehicules/vehiculePage.php
+++ views/partials/vehicules/vehiculePage.php
@@ -250,14 +250,14 @@ try {
             <div class="col-lg-3 col-md-6 mb-5">
                 <h4 class="text-uppercase text-light mb-4">Get In Touch</h4>
                 <p class="mb-2"><i class="fa fa-map-marker-alt text-white mr-3"></i>123 Street, New York, USA</p>
-                <p class="mb-2"><i class="fa fa-phone-alt text-white mr-3"></i>+012 345 67890</p>
-                <p><i class="fa fa-envelope text-white mr-3"></i>info@example.com</p>
+                <p class="mb-2"><i class="fa fa-phone-alt text-white mr-3"></i>+212 6933 05050</p>
+                <p><i class="fa fa-envelope text-white mr-3"></i>jadthegamer06@gmail.com</p>
                 <h6 class="text-uppercase text-white py-2">Follow Us</h6>
                 <div class="d-flex justify-content-start">
-                    <a class="btn btn-lg btn-dark btn-lg-square mr-2" href="#"><i class="fab fa-twitter"></i></a>
-                    <a class="btn btn-lg btn-dark btn-lg-square mr-2" href="#"><i class="fab fa-facebook-f"></i></a>
-                    <a class="btn btn-lg btn-dark btn-lg-square mr-2" href="#"><i class="fab fa-linkedin-in"></i></a>
-                    <a class="btn btn-lg btn-dark btn-lg-square" href="#"><i class="fab fa-instagram"></i></a>
+                    <a class="btn btn-lg btn-dark btn-lg-square mr-2" href="https://github.com/Jad-Bel"><i class="fab fa-github"></i></a>
+                    <a class="btn btn-lg btn-dark btn-lg-square mr-2" href="https://www.linkedin.com/in/jad-belassiria-16390321b/"><i class="fab fa-linkedin-in"></i></a>
+                    <a class="btn btn-lg btn-dark btn-lg-square mr-2" href="https://www.facebook.com/profile.php?id=100070651475648"><i class="fab fa-facebook-f"></i></a>
+                    <a class="btn btn-lg btn-dark btn-lg-square" href="https://www.instagram.com/_69eei/"><i class="fab fa-instagram"></i></a>
                 </div>
             </div>
             <div class="col-lg-3 col-md-6 mb-5">
@@ -275,22 +275,22 @@ try {
                 <h4 class="text-uppercase text-light mb-4">Car Gallery</h4>
                 <div class="row mx-n1">
                     <div class="col-4 px-1 mb-2">
-                        <a href=""><img class="w-100" src="../../../img/gallery-1.jpg" alt=""></a>
+                        <a href=""><img class="w-100" src="../../img/gallery-1.jpg" alt=""></a>
                     </div>
                     <div class="col-4 px-1 mb-2">
-                        <a href=""><img class="w-100" src="../../../img/gallery-2.jpg" alt=""></a>
+                        <a href=""><img class="w-100" src="../../img/gallery-2.jpg" alt=""></a>
                     </div>
                     <div class="col-4 px-1 mb-2">
-                        <a href=""><img class="w-100" src="../../../img/gallery-3.jpg" alt=""></a>
+                        <a href=""><img class="w-100" src="../../img/gallery-3.jpg" alt=""></a>
                     </div>
                     <div class="col-4 px-1 mb-2">
-                        <a href=""><img class="w-100" src="../../../img/gallery-4.jpg" alt=""></a>
+                        <a href=""><img class="w-100" src="../../img/gallery-4.jpg" alt=""></a>
                     </div>
                     <div class="col-4 px-1 mb-2">
-                        <a href=""><img class="w-100" src="../../../img/gallery-5.jpg" alt=""></a>
+                        <a href=""><img class="w-100" src="../../img/gallery-5.jpg" alt=""></a>
                     </div>
                     <div class="col-4 px-1 mb-2">
-                        <a href=""><img class="w-100" src="../../../img/gallery-6.jpg" alt=""></a>
+                        <a href=""><img class="w-100" src="../../img/gallery-6.jpg" alt=""></a>
                     </div>
                 </div>
             </div>
@@ -310,9 +310,9 @@ try {
         </div>
     </div>
     <div class="container-fluid bg-dark py-4 px-sm-3 px-md-5">
-        <p class="mb-2 text-center text-body">&copy; <a href="#">Your Site Name</a>. All Rights Reserved.</p>
-
-        <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
+        <p class="mb-2 text-center text-body">&copy; <a href="#">Drive-Loc</a>. All Rights Reserved.</p>
+		
+		<!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->					
         <p class="m-0 text-center text-body">Designed by <a href="https://htmlcodex.com">HTML Codex</a></p>
     </div>
     <!-- Footer End -->
